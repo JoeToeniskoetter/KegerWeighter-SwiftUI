28c69bf4785634553dec80bfa43eaebb82c32d48
JoeToeniskotter <jtoeniskoetter.jt@gmail.com>
2021-11-30 07:55:57 -0600

    Initial Commit

---
+++ KegerWeighter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KegerWeighter
+//
+//  Created by Joe Toeniskoetter on 11/30/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KegerWeighter/KegerWeighterApp.swift
@@ -0,0 +1,17 @@
+//
+//  KegerWeighterApp.swift
+//  KegerWeighter
+//
+//  Created by Joe Toeniskoetter on 11/30/21.
+//
+
+import SwiftUI
+
+@main
+struct KegerWeighterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
